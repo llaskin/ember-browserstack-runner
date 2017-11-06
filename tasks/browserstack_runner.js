a5b4a4a4db94f42e74245322ebc9b4f5706ad4aa
llaskin <llaskin@gmail.com>
2017-11-06 12:53:17 -0600

    More Saucelabs updates

--- tasks/browserstack_runner.js
+++ tasks/browserstack_runner.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec;
 
 module.exports = function(grunt) {
-  grunt.registerTask('browserstack_runner','Your task description goes here.', function() {
+  grunt.registerTask('ember_saucelabs_runner','Your task description goes here.', function() {
     var done = this.async();
 
     // Merge task-specific and/or target-specific options with these defaults.
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
       separator: ', '
     });
 
-    var subProcess = exec('bash -c "PATH=$PATH:$(npm bin) browserstack-runner"', function (err, stdout, stderr) {
+    var subProcess = exec('bash -c "PATH=$PATH:$(npm bin) ember_saucelabs_runner"', function (err, stdout, stderr) {
       done(err ? false : true);
     });
 
